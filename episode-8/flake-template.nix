1c91b60059e95949bc1b7ba39e0b6a46ce376da0
Jason M <jason@whydoesnothing.work>
2024-05-07 22:48:35 -0600

    adding episode 8 flake template contains overlay for using multiple channels for packages

---
+++ episode-8/flake-template.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-unstable, ... }: {
+    nixosConfigurations.demo = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ({ pkgs, ... }: {
+          nixpkgs = {
+            overlays = [(self: super: { unstable = import nixpkgs-unstable { system = "x86_64-linux"; }; })
+            ];
+          };
+        })
+        ./configuration.nix
+      ];
+    };
+  };
+}
