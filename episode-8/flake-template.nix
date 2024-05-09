0c9c6534d9fcb10be8e2e08d4232cec84521cb90
Jason M <jason@whydoesnothing.work>
2024-05-09 08:27:32 -0600

    fixed main template

--- episode-8/flake-template.nix
+++ episode-8/flake-template.nix
@@ -4,7 +4,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, ... }: {
+  outputs = { self, nixpkgs, ... }: {
     nixosConfigurations.demo = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
