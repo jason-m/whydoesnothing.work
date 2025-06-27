06bcec7a42cc961bb3289b536fd8f671cbeadd83
Jason <jason@whydoesnothing.work>
2025-06-27 14:30:31 -0600

    added transfer secret

--- episode-14/scripts/ocis_gen_secrets.nix
+++ episode-14/scripts/ocis_gen_secrets.nix
@@ -5,3 +5,4 @@ echo
 echo "OCIS_JWT_SECRET=$(openssl rand -base64 48 | tr -d '=+/' | cut -c1-64)"
 echo "OCIS_MACHINE_AUTH_API_KEY=$(openssl rand -base64 36 | tr -d '=+/' | cut -c1-48)"
 echo "OCIS_ADMIN_USER_PASSWORD=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 24)"
+echo "OCIS_TRANSFER_SECRET=$(openssl rand -base64 48 | tr -d '=+/' | cut -c1-64)"
